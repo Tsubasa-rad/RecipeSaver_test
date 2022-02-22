e239da8bfbaa2f3a7fa1cdfae139254953ee0b33
Tsubasa-rad <tyamamoto472@gmail.com>
2022-02-22 15:18:07 +0900

    Initial Commit

---
+++ RecipeSaver/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RecipeSaver
+//
+//  Created by 山本翼 on 2022/02/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RecipeSaver/RecipeSaverApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeSaverApp.swift
+//  RecipeSaver
+//
+//  Created by 山本翼 on 2022/02/22.
+//
+
+import SwiftUI
+
+@main
+struct RecipeSaverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
